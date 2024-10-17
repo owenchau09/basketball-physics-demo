4ab6a65d382d4b819a85e97c6399f91bfc435995
Owen Chau <ochau278@student.fuhsd.org>
2024-10-17 13:30:12 -0700

    a

--- Lab-Unit2-PhysicsShapeDemo/src/DrawingSurface.java
+++ Lab-Unit2-PhysicsShapeDemo/src/DrawingSurface.java
@@ -1,11 +1,21 @@
 
+import java.awt.Color;
+
+import agandhi523.sheeps.Rectangle;
 import processing.core.PApplet;
 
 
 public class DrawingSurface extends PApplet {
 
+	private PhysicsShape shape, holding;
+	
+	
 	public DrawingSurface() {
-		
+		shape = new PhysicsShape(new Rectangle(100, 100, 50, 50));
+		shape.color(Color.WHITE);
+		shape.stroker(Color.BLACK);
+		shape.strokePower(5);
+		holding = null;
 	}
 	
 	public void settings() {
@@ -23,13 +33,25 @@ public class DrawingSurface extends PApplet {
 	// sequence and after the last line is read, the first 
 	// line is executed again.
 	public void draw() {
+		
+		shape.act();
+		
 		background(255);
-		
-		
+		shape.draw(this);
 	}
 	
 	public void mousePressed() {
-		
+		if(shape.isPointInside(mouseX, mouseY)) {
+//			shape.accelerate(0.5, 0);
+			holding = shape;
+		}
+	}
+	
+	public void mouseReleased() {
+		if(holding != null) {
+			holding.accelerate(mouseX-pmouseX, mouseY-pmouseY);
+			holding = null;
+		}
 	}
 	
 	
--- Lab-Unit2-PhysicsShapeDemo/src/PhysicsShape.java
+++ Lab-Unit2-PhysicsShapeDemo/src/PhysicsShape.java
@@ -1,11 +1,19 @@
+import java.awt.Color;
+
+import agandhi523.sheeps.Shape;
 import processing.core.PApplet;
 
 public class PhysicsShape {
 
 	private Shape s;
 	
+	private double vx, vy;
+	private final double gravity = 5;
+	
 	public PhysicsShape(Shape s) {
 		this.s = s;
+		vx = 0;
+		vy = 0;
 	}
 	
 	
@@ -14,4 +22,30 @@ public class PhysicsShape {
 		s.draw(surface);
 	}
 	
+	public void act() {
+		s.setPositionX(s.getX() + vx);
+		s.setPositionY(s.getY() + vy);
+		
+	}
+	
+	public void accelerate(double cvelx, double cvely) {
+		vx += cvelx;
+		vy = (vy + cvely + gravity);
+	}
+	
+	public void color(Color color) {
+		s.color(color);
+	}
+	
+	public void stroker(Color color) {
+		s.stroker(color);
+	}
+	
+	public void strokePower(int power) {
+		s.strokePower(power);
+	}
+	
+	public boolean isPointInside(double x, double y) {
+		return s.isPointInside(x, y);
+	}
 }
