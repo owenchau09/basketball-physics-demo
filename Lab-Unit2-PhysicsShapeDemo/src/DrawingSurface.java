3b241c0350ab7b514ff3ffe6176ed9c692419890
Owen Chau <ochau278@student.fuhsd.org>
2024-10-24 22:10:01 -0700

    final revision 10/24

--- Lab-Unit2-PhysicsShapeDemo/src/DrawingSurface.java
+++ Lab-Unit2-PhysicsShapeDemo/src/DrawingSurface.java
@@ -1,114 +1,136 @@
-
 import java.awt.Color;
 import java.util.ArrayList;
-import java.util.Collections;
-
 import agandhi523.sheeps.Circle;
 import agandhi523.sheeps.Line;
 import agandhi523.sheeps.Rectangle;
-import agandhi523.sheeps.Shape;
 import processing.core.PApplet;
 
 
 public class DrawingSurface extends PApplet {
 	
-	private PhysicsShape holding, basketball;
-	private Rectangle backboard, hoop, top, left, right, bottom, c1, c2, c3;
+	private PhysicsShape holding, basketball, holdingb;
+	private Rectangle backboard, hoop, top, left, right, bottom, c1, c2, c3, leftedge;
 	private Line connector3, net1, net2, net3, net4, neth1, neth2, neth3, neth4;
 	private ArrayList<PhysicsShape> balls = new ArrayList<PhysicsShape>();
-	private ArrayList<Double> cx = new ArrayList<Double>(Collections.nCopies(100, 0.0));
-	private ArrayList<Double> cy = new ArrayList<Double>(Collections.nCopies(100, 0.0));
+	private ArrayList<PhysicsShape> boxes = new ArrayList<PhysicsShape>();
+	private ArrayList<Double> cx = new ArrayList<Double>();
+	private ArrayList<Double> cy = new ArrayList<Double>();
 	private ArrayList<Boolean> hold = new ArrayList<Boolean>();
+	private ArrayList<Double> pMouseX = new ArrayList<Double>();
+	private ArrayList<Double> pMouseY = new ArrayList<Double>();
+	private ArrayList<Double> cxb = new ArrayList<Double>();
+	private ArrayList<Double> cyb = new ArrayList<Double>();
+	private ArrayList<Boolean> holdb = new ArrayList<Boolean>();
+	private ArrayList<Double> pMouseXb = new ArrayList<Double>();
+	private ArrayList<Double> pMouseYb = new ArrayList<Double>();
+	private int mode = 0;
+	
 	
 	public DrawingSurface() {
 		basketball = new PhysicsShape(new Circle(100, 100, 20));
-		basketball.color(Color.ORANGE);
+		basketball.color(new Color(250,131,32));
 		basketball.stroker(Color.BLACK);
 		basketball.strokePower(5);
 		balls.add(basketball);
+		hold.add(false);
+		cx.add(0.0);
+		cy.add(0.0);
+		pMouseX.add(null);
+		pMouseY.add(null);
 		holding = null;
 
-		backboard = new Rectangle(800-15-6, 300-90, 15, 90);
+		double s = 2.0;
+
+		backboard = new Rectangle(800-15*s, 300-90*s, 15*s-2*s, 90*s);
 		backboard.color(Color.WHITE);
 		backboard.stroker(Color.BLACK);
 		backboard.strokePower(3);
 		
-		c1 = new Rectangle(800-15-6-30, 2.0/3*90+300-90, 30, 0);
-		c1.color(Color.WHITE);
-		c1.stroker(Color.BLACK);
+		c1 = new Rectangle(800-15*s-30*s, 2.0/3*90*s+300-90*s, 30*s, 0);
+		c1.color(Color.RED);
+		c1.stroker(Color.RED);
 		c1.strokePower(1);
 		
-		c2 = new Rectangle(800-15-6-15, 2.0/3*90+300-90+15, 15, 0);
-		c2.color(Color.WHITE);
-		c2.stroker(Color.BLACK);
+		c2 = new Rectangle(800-15*s-15*s, 2.0/3*90*s+300-90*s+15*s, 15*s, 0);
+		c2.color(Color.RED);
+		c2.stroker(Color.RED);
 		c2.strokePower(1);
 		
-		c3 = new Rectangle(800-15-6-15, 2.0/3*90+300-90+15, 15, 0);
-		c3.color(Color.WHITE);
-		c3.stroker(Color.BLACK);
+		c3 = new Rectangle(800-15*s-15*s, 2.0/3*90*s+300-90*s+15*s, 15*s, 0);
+		c3.color(Color.RED);
+		c3.stroker(Color.RED);
 		c3.strokePower(1);
 		
-		connector3 = new Line(800-15-6-15, 2.0/3*90+300-90+15, 800-15-6-30, 2.0/3*90+300-90+5);
-		connector3.color(Color.WHITE);
-		connector3.stroker(Color.BLACK);
+		connector3 = new Line(800-15*s-15*s, 2.0/3*90*s+300-90*s+15*s, 800-15*s-30*s, 2.0/3*90*s+300-90*s+5*s);
+		connector3.color(Color.RED);
+		connector3.stroker(Color.RED);
 		connector3.strokePower(1);
 		
-		hoop = new Rectangle(800-15-6-30-50, 2.0/3*90+300-90, 50, 5);
-		hoop.color(Color.WHITE);
+		hoop = new Rectangle(800-15*s-30*s-50*s, 2.0/3*90*s+300-90*s, 50*s, 5*s);
+		hoop.color(Color.RED);
 		hoop.stroker(Color.BLACK);
 		hoop.strokePower(1);
 		
-		net1 = new Line(800-15-6-30-50+2, 2.0/3*90+300-90+5, 800-15-6-30-50+7, 2.0/3*90+300-90+40+5);
+		leftedge = new Rectangle(800-15*s-30*s-50*s, 2.0/3*90*s+300-90*s, 0, 5*s);
+		leftedge.color(Color.BLUE);
+		leftedge.stroker(Color.BLACK);
+		leftedge.strokePower(1);
+		
+		
+		net1 = new Line(800-15*s-30*s-50*s+2*s, 2.0/3*90*s+300-90*s+5, 800-15*s-30*s+7*s-50*s, 2.0/3*90*s+300-90*s+40*s+5);
 		net1.color(Color.WHITE);
 		net1.stroker(Color.BLACK);
 		net1.strokePower(1);
-				
-		net2 = new Line(800-15-6-30-50+50-2, 2.0/3*90+300-90+5, 800-15-6-30-50+50-7, 2.0/3*90+300-90+40+5);
+		
+		net2 = new Line(800-15*s-30*s+50*s-50*s-2*s, 2.0/3*90*s+300-90*s+5, 800-15*s-50*s-30*s+50*s-7*s, 2.0/3*90*s+300-90*s+40*s+5);
 		net2.color(Color.WHITE);
 		net2.stroker(Color.BLACK);
 		net2.strokePower(1);
 		
-		net3 = new Line(800-15-6-30-50+50*3.5/12, 2.0/3*90+300-90+5, 800-15-6-30-50+50*3.5/12+3, 2.0/3*90+300-90+40+5);
+		net3 = new Line(800-15*s-50*s-30*s+50*3.5/12*s, 2.0/3*90*s+300-90*s+5, 800-15*s-50*s-30*s+50*3.5/12*s+3*s, 2.0/3*90*s+300-90*s+40*s+5);
 		net3.color(Color.WHITE);
 		net3.stroker(Color.BLACK);
 		net3.strokePower(1);
 		
-		net4 = new Line(800-15-6-30-50+50-50*3.5/12, 2.0/3*90+300-90+5, 800-15-6-30-50+50-50*3.5/12-3, 2.0/3*90+300-90+40+5);
+		net4 = new Line(800-15*s-30*s-50*s+50*s-50*3.5/12*s, 2.0/3*90*s+300-90*s+5, 800-15*s-30*s-50*s+50*s-50*3.5/12*s-3*s, 2.0/3*90*s+300-90*s+40*s+5);
 		net4.color(Color.WHITE);
 		net4.stroker(Color.BLACK);
 		net4.strokePower(1);
-		
-		//	y = 8x - 5333
-		neth1 = new Line(800-15-6-30-50+2+1, 8*(800-15-6-30-50+2+1)-5333, 746, 8*(800-15-6-30-50+2+1)-5333);
+//		y = 8x - 4667 for 1st diag
+//		y = -8x + 5,893 for 2nd diag
+		neth1 = new Line(800-15*s-30*s-50*s+2*s+2, 8*(800-15*s-30*s-50*s+2*s+2)-4667, 704, 8*(800-15*s-30*s-50*s+2*s+2)-4667);
 		neth1.color(Color.WHITE);
 		neth1.stroker(Color.BLACK);
 		neth1.strokePower(1);
 		
-		neth2 = new Line(800-15-6-30-50+2+2, 8*(800-15-6-30-50+2+2)-5333, 745, 8*(800-15-6-30-50+2+2)-5333);
+		neth2 = new Line(800-15*s-30*s-50*s+2*s+4, 8*(800-15*s-30*s-50*s+2*s+4)-4667, 702, 8*(800-15*s-30*s-50*s+2*s+4)-4667);
 		neth2.color(Color.WHITE);
 		neth2.stroker(Color.BLACK);
 		neth2.strokePower(1);
 		
-		neth3 = new Line(800-15-6-30-50+2+3, 8*(800-15-6-30-50+2+3)-5333, 744, 8*(800-15-6-30-50+2+3)-5333);
+		neth3 = new Line(800-15*s-30*s-50*s+2*s+6, 8*(800-15*s-30*s-50*s+2*s+6)-4667, 700, 8*(800-15*s-30*s-50*s+2*s+6)-4667);
 		neth3.color(Color.WHITE);
 		neth3.stroker(Color.BLACK);
 		neth3.strokePower(1);
 		
-		neth4 = new Line(800-15-6-30-50+2+4, 8*(800-15-6-30-50+2+4)-5333, 743, 8*(800-15-6-30-50+2+4)-5333);
+		neth4 = new Line(800-15*s-30*s-50*s+2*s+8, 8*(800-15*s-30*s-50*s+2*s+8)-4667, 698, 8*(800-15*s-30*s-50*s+2*s+8)-4667);
 		neth4.color(Color.WHITE);
 		neth4.stroker(Color.BLACK);
 		neth4.strokePower(1);
+
+
+
 		
 		top = new Rectangle(0, 0, 800, 0);
-		bottom = new Rectangle(0, 600, 800, 0);
+		bottom = new Rectangle(0, 600, 800, -5);
 		left = new Rectangle(0, 0, 0, 600);
 		right = new Rectangle(800, 0, 0, 600);
 		top.color(Color.WHITE);
 		top.stroker(Color.BLACK);
 		top.strokePower(1);
-		bottom.color(Color.WHITE);
-		bottom.stroker(Color.BLACK);
-		bottom.strokePower(1);
+		bottom.color(new Color(153, 0, 2));
+		bottom.stroker(new Color(153, 0, 2));
+		bottom.strokePower(5);
 		left.color(Color.WHITE);
 		left.stroker(Color.BLACK);
 		left.strokePower(1);
@@ -137,8 +159,15 @@ public class DrawingSurface extends PApplet {
 
 		
 		for(int i = 0; i < balls.size(); i++) {
-			balls.get(i).act(top, bottom, left, right, c1, c2, backboard, cx.get(i), cy.get(i));
+			balls.get(i).act(top, bottom, left, right, c1, c2, backboard, cx.get(i), cy.get(i), hoop, leftedge);
 			balls.get(i).draw(this);
+		}
+		
+		if(boxes.size() > 0) {
+			for(int i = 0; i < boxes.size(); i++) {
+				boxes.get(i).act(top, bottom, left, right, c1, c2, backboard, cxb.get(i), cyb.get(i), hoop, leftedge);
+				boxes.get(i).draw(this);
+			}
 		}
 		
 		
@@ -148,7 +177,6 @@ public class DrawingSurface extends PApplet {
 		c2.draw(this);
 		c3.draw(this);
 		
-		hoop.draw(this);
 		net1.draw(this);
 		net2.draw(this);
 		net3.draw(this);
@@ -157,35 +185,97 @@ public class DrawingSurface extends PApplet {
 		neth2.draw(this);
 		neth3.draw(this);
 		neth4.draw(this);
+		hoop.draw(this);
 		
 //		top.draw(this);
-//		bottom.draw(this);
+		bottom.draw(this);
 //		left.draw(this);
 //		right.draw(this);
+		
+//		leftedge.draw(this);
+		textSize(16);
+		fill(0);
+		text("Press c for ball mode and r for box mode!\nHold mouse on a ball/box to move it around. The further you drag it, the faster it'll go (gravity affected). \nClick outside of the object to spawn a new ball/box. Try to score on the hoop!" + "\nCurrent Score: " + PhysicsShape.score, 30, 40);
 	}
 	
 	public void mousePressed() {
-		for(int i = 0; i < balls.size(); i++) {
-			if(balls.get(i).isPointInside(mouseX, mouseY)) {
-				holding = balls.get(i);
-				return;
+		if(mode == 0) {
+			for(int i = 0; i < balls.size(); i++) {
+				if(balls.get(i).isPointInside(mouseX, mouseY)) {
+					holding = balls.get(i);
+					hold.set(i, true);
+					pMouseX.set(i, (double) mouseX);
+					pMouseY.set(i, (double) mouseY);
+					return;
+				}
 			}
+			PhysicsShape ball = new PhysicsShape(new Circle(mouseX, mouseY, 20));
+			ball.color(new Color(250,131,32));
+			ball.stroker(Color.BLACK);
+			ball.strokePower(5);
+			balls.add(ball);
+			hold.add(false);
+			cx.add(0.0);
+			cy.add(0.0);
+			pMouseX.add((double) mouseX);
+			pMouseY.add((double) mouseY);
 		}
-		PhysicsShape ball = new PhysicsShape(new Circle(mouseX, mouseY, 20));
-		ball.color(Color.ORANGE);
-		ball.stroker(Color.BLACK);
-		ball.strokePower(3);
-		balls.add(ball);
+		else if(mode == 1) {
+			for(int i = 0; i < boxes.size(); i++) {
+				if(boxes.get(i).isPointInside(mouseX, mouseY)) {
+					holdingb = boxes.get(i);
+					holdb.set(i, true);
+					pMouseXb.set(i, (double) mouseX);
+					pMouseYb.set(i, (double) mouseY);
+					return;
+				}
+			}
+			PhysicsShape box = new PhysicsShape(new Rectangle(mouseX, mouseY, 50, 50));
+			box.color(Color.CYAN);
+			box.stroker(Color.BLACK);
+			box.strokePower(5);
+			boxes.add(box);
+			holdb.add(false);
+			cxb.add(0.0);
+			cyb.add(0.0);
+			pMouseXb.add((double) mouseX);
+			pMouseYb.add((double) mouseY);
+		}
+	}
+	
+	public void keyPressed() {
+		if(key == 'c') {
+			mode = 0;
+		}
+		if(key == 'r') {
+			mode = 1;
+		}
 	}
 	
 	public void mouseReleased() {
-		for(int i = 0; i < balls.size(); i++) {
-			if(hold.get(i) == true) {
-				cx.set(i, (double) (mouseX-pmouseX));
-				cy.set(i, (double) (mouseY-pmouseY));
-				holding = null;
+		if(mode == 0) {
+			for(int i = 0; i < balls.size(); i++) {
+				if(hold.get(i) == true) {
+					cx.set(i, (double) (mouseX-pMouseX.get(i)));
+					cy.set(i, (double) (mouseY-pMouseY.get(i)));
+					hold.set(i, false);
+					balls.get(i).accelerate(cx.get(i), cy.get(i));
+					holding = null;
+				}
+				
 			}
-			
+		}
+		else if(mode == 1) {
+			for(int i = 0; i < boxes.size(); i++) {
+				if(holdb.get(i) == true) {
+					cxb.set(i, (double) (mouseX-pMouseXb.get(i)));
+					cyb.set(i, (double) (mouseY-pMouseYb.get(i)));
+					holdb.set(i, false);
+					boxes.get(i).accelerate(cxb.get(i), cyb.get(i));
+					holdingb = null;
+				}
+				
+			}
 		}
 	}
 	
--- Lab-Unit2-PhysicsShapeDemo/src/PhysicsShape.java
+++ Lab-Unit2-PhysicsShapeDemo/src/PhysicsShape.java
@@ -8,10 +8,10 @@ import processing.core.PApplet;
 
 public class PhysicsShape {
 
-	private Shape s, temp;
-	
+	private Shape s, tempHitbox;
+	public static int score = 0;
 	private double vx, vy;
-	private final double gravity = 1.2;
+	private final double gravity = 0.07;
 	
 	public PhysicsShape(Shape s) {
 		this.s = s;
@@ -25,8 +25,8 @@ public class PhysicsShape {
 //		temp.draw(surface);
 	}
 	
-	public void act(Rectangle top, Rectangle bottom, Rectangle left, Rectangle right, Rectangle c1, Rectangle c2, Rectangle backboard, double cx, double cy) {
-		accelerate(cx, cy);
+	public void act(Rectangle top, Rectangle bottom, Rectangle left, Rectangle right, Rectangle c1, Rectangle c2, Rectangle backboard, double cx, double cy, Rectangle hoop, Rectangle leftedge) {
+		accelerate(0, 0);
 		s.setPositionX(s.getX() + vx);
 		s.setPositionY(s.getY() + vy);
 		
@@ -35,27 +35,80 @@ public class PhysicsShape {
 			Circle c = (Circle) s;
 			double radius = c.getPerimeter()/(2*Math.PI);
 			Rectangle hitbox = new Rectangle(c.getX()-radius, c.getY()-radius, 2*radius, 2*radius);
-			temp = hitbox;
-			temp.color(Color.WHITE);
-			temp.stroker(Color.BLACK);
-			temp.strokePower(1);
+			tempHitbox = hitbox;
+			tempHitbox.color(Color.WHITE);
+			tempHitbox.stroker(Color.BLACK);
+			tempHitbox.strokePower(1);
 //			System.out.println((hitbox.isTouching(top) || hitbox.isTouching(bottom) || hitbox.isTouching(left) || hitbox.isTouching(right)));
 			
 			if(hitbox.isTouching(top) || hitbox.isTouching(c2)) {
 				s.setPositionY(s.getY() + 5);
-			    vy *= -0.8;
+			    vy *= -0.7;
 			}
 			if(hitbox.isTouching(bottom) || hitbox.isTouching(c1)) {
 				s.setPositionY(s.getY() - 5);
-			    vy *= -0.8;
+			    vy *= -0.7;
+			    if(vx == 0 && hitbox.isTouching(c1)) {
+			    	vx = -1;
+			    }
 			}
 			if(hitbox.isTouching(left)) {
 				s.setPositionX(s.getX() + 5);
-			    vx *= -0.8; 
+			    vx *= -0.7; 
 			}
-			if(hitbox.isTouching(right) || hitbox.isTouching(backboard)) {
+			if(hitbox.isTouching(right) || hitbox.isTouching(backboard) || leftedge.isTouching(hitbox)) {
+				if(leftedge.isTouching(hitbox)) {
+					if(c.getX() >= leftedge.getX()) {
+						vx += 2;
+						s.setPositionX(s.getX() + 15);
+					}
+					else if(c.getX() < leftedge.getX()) {
+						vx -= 2;
+					}
+				}
 				s.setPositionX(s.getX() - 5);
-			    vx *= -0.8; 
+			    vx *= -0.7; 
+			}
+			
+			if(hoop.isPointInside(c.getX(), c.getY())) {
+				score += 1;
+			}
+		}
+		
+		else if(s instanceof Rectangle) {
+//				System.out.println((hitbox.isTouching(top) || hitbox.isTouching(bottom) || hitbox.isTouching(left) || hitbox.isTouching(right)));
+			
+			if(s.isTouching(top) || s.isTouching(c2)) {
+				s.setPositionY(s.getY() + 5);
+			    vy *= -0.7;
+			}
+			if(s.isTouching(bottom) || s.isTouching(c1)) {
+			    vy *= -0.7;
+			    if(vx == 0 && s.isTouching(c1)) {
+			    	vx = -1;
+			    }
+			    s.setPositionY(s.getY() - 5);
+			}
+			if(s.isTouching(left)) {
+				s.setPositionX(s.getX() + 5);
+			    vx *= -0.7; 
+			}
+			if(s.isTouching(right) || s.isTouching(backboard) || leftedge.isTouching(s)) {
+				if(leftedge.isTouching(s)) {
+					if(s.getX() >= leftedge.getX()) {
+						vx += 2;
+						s.setPositionX(s.getX() + 15);
+					}
+					else if(s.getX() < leftedge.getX()) {
+						vx -= 2;
+					}
+				}
+				s.setPositionX(s.getX() - 5);
+			    vx *= -0.7; 
+			}
+			
+			if(hoop.isPointInside(s.getX(), s.getY())) {
+				score += 1;
 			}
 		}
 		
@@ -63,8 +116,9 @@ public class PhysicsShape {
 	}
 	
 	public void accelerate(double cvelx, double cvely) {
-		vx = (vx + cvelx*1.1);
-		vy = (-vy + cvely*1.1 + gravity);
+		vx += cvelx*0.08;
+		vy += gravity;
+		vy += cvely*0.08;
 	}
 	
 	public void color(Color color) {
